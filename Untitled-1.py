b3c750bb77e25f0fe32217079617a3ff1e7c1785
Amirhosseinsharaf <92889935+amir6500@users.noreply.github.com>
2025-01-19 11:17:23 +0000

    Add script to clone and run Anonymous-SMS project

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+ from gettext import install
+from pdb import main
+from turtle import clone
+import git
+import pip
+from pkg_resources import Requirement
+from sympy import python
+from torch import R, ScriptModuleSerializer
+
+
+git clone https://github.com/AbdulRKB/Anonymous-SMS.git
+import shutil
+
+shutil.rmtree('Anonymous-SMS')
+pip install -R Requirement.txt
+ python main.py
